2055f349a8bd6d56a5a5be17b9e4a37551673f4f
omargibreel <omarosama1124@gmail.com>
2026-02-03 00:30:21 +0200

    create navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { FlowbiteService } from './../../../core/services/flowbite.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {
-
+export class NavbarComponent implements OnInit {
+  constructor(private FlowbiteService: FlowbiteService) {}
+  @Input() isLogin: boolean = false;
+  ngOnInit(): void {
+    this.FlowbiteService.loadFlowbite((flowbite) => {
+      initFlowbite();
+    });
+  }
 }
